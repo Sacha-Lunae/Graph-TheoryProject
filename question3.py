b3eda91c98627c73e224f58bf6bfc937e2392425
Adriaan0111 <82849327+Edran0111@users.noreply.github.com>
2023-04-09 17:48:29 +0200

    Fonction cycle faite et commentée

--- question3.py
+++ question3.py
@@ -3,7 +3,7 @@ from question1 import *
 
 
 
-matrix = getAdjacencyMatrix(getConsTable(1))
+matrix = getAdjacencyMatrix(getConsTable(12))
 countrowcol= []
 
 def initlabelrowcol(countrowcol, adj_matrix):
@@ -50,6 +50,8 @@ def remove_rowcol(adj_matrix, finderrow, findercol):
 
 
 def remove_zeros(matrix):
+    #here is the attribution of the variables
+    #running if for the while loop
     running=True
     countrowcol = []
     countrowcol = initlabelrowcol(countrowcol, matrix)
@@ -59,93 +61,90 @@ def remove_zeros(matrix):
 
 
     while running:
-        try:
-            print("NEW ITERATION")
-            adj_matrix = matrix
-            
 
-            num_rows = len(adj_matrix)
-            num_cols = len(adj_matrix[0])
-            finderrow = []
-            findercol = []
-
-            for col in range(num_cols):
-                if col in findercol:
-                    continue
-                # Check if the column has only 0's
-                col_has_zeros = all(adj_matrix[row][col] == 0 for row in range(num_rows))
-                if col_has_zeros:
-                    findercol.append(col)
-
-            for row in range(num_rows):
-                row_has_zeros = all(adj_matrix[row][col] == 0 for col in range(num_cols))
-                if row_has_zeros:
-                    finderrow.append(row)
-
-
-            # print("finderrow (row erased):", finderrow, )
-            # print("findercol (column erased):", findercol, )
-            for i in range(len(findercol)):
-                finderrow.append(findercol[i])
-            for i in range(len(finderrow)):
-                findercol.append(finderrow[i])
-            finderrow = list(set(finderrow + findercol))
-            findercol = list(set(findercol + finderrow))
-
-            #print("finderrow (row erased):", finderrow, )
-            print("findercol (column erased):", findercol, n ,)
-            if findercol== []:
-                running = False
-
-            test.append(findercol)
-
-
-            if len(finderrow) == 0:
-                running=False
-                print("oute")
-            if len(finderrow)<1:
-                print("AHHHAHAHAHHAHAAHHAH")
-                return matrix
-            else:
-                adj_matrix = remove_rowcol(adj_matrix, finderrow, findercol)
-                print("new matrix :")
-                for i in adj_matrix:
-                    print(i)
-
-        except IndexError:
-            print("ATTENTION")
+        columns_erased=0
+        #print("new matrix :")
+        #we attribute the parameter of the function to a variable
+        adj_matrix = matrix
+        #we print the matrix
+        for i in adj_matrix:
+            print(i)
+        #if the matrix is a 0(1 row and column) , or only a 1, it will return false
+        if adj_matrix == [[0]]or adj_matrix==[[1]]:
+            print("connection terminated")
             return False
-        pass
+        #we attribute the counters for the next parts
+        num_rows = len(adj_matrix)
+        num_cols = len(adj_matrix[0])
+        finderrow = []
+        findercol = []
 
+        #these two loops will gather where is located the deleted row or column
+        for col in range(num_cols):
+            col_has_zeros = all(adj_matrix[row][col] == 0 for row in range(num_rows))#the all function will detect the columns that are full of 0
+            if col_has_zeros:
+                findercol.append(col)#we will remember them if we append in the findercol variable
+        for row in range(num_rows):
+            row_has_zeros = all(adj_matrix[row][col] == 0 for col in range(num_cols))#the all function will detect the rows that are full of 0
+            if row_has_zeros:
+                finderrow.append(row)#same here
+
+        #for these two "for" conditions, we will fuse the informations of the columns and rows of 0 so that we can erase them later in the function remove_rowcol
+        #IE: the finderrow and findercol are always equal because we are in a square matrix
+        for i in range(len(findercol)):
+            finderrow.append(findercol[i])
+            columns_erased+=1#we are loggin the informations if a row or a columns is going to be erased
+        for i in range(len(finderrow)):
+            findercol.append(finderrow[i])
+            columns_erased+=1#the logging is the same here
+
+        finderrow = list(set(finderrow + findercol))
+        findercol = list(set(findercol + finderrow))
+
+        #this line is a debug line
+        #print("findercol (column erased):", findercol)
+
+        #now we append all of the columns/row that have been erased so that we can review them after
+        test.append(findercol)
+        #this is just a small check here if something gets wrong
+        if adj_matrix == [0]:
+            return False
+
+        else:#this is where the columns and rows are erased simulaneousely
+            adj_matrix = remove_rowcol(adj_matrix, finderrow, findercol)
+            if columns_erased==0:#now the columns_erased are put in good use here, if there is a cycle, so that means we can break out of the infinite loop because no modification occured
+                break#we got out of the algorithm now
+
+    #initialisation of the dictionnary so that we have a mark of which columns/row is erased, the dictionnary will look like : {0:0, 1:1 [and so on]}
     for i in range(len(cpt)):
         my_dict[cpt[i]] = i
+    #this is initializing the test function into an ascending order of the 2d list
     for sublist in test:
         sublist.sort(reverse=True)
 
-    # Erasing the value inside of the dictionary and re-initializing the values in ascending order
-    for sublist in test:
-
-        for item in sublist:
-
+    # This loop is erasing the value inside of the dictionary and after that it re-initialize the values in ascending order
+    for sublist in test:#access the first dimension of the list
+        for item in sublist:#accession the second dimension of the list, so that we can access the periodicly log of the erased columns
             if item in my_dict.values():
                 key_to_delete = [key for key in my_dict if my_dict[key] == item][0]
-                del my_dict[key_to_delete]
+                del my_dict[key_to_delete]#we delete the searched key
+                #we now do a loop to reorganize the dictionnary, so we have ascending values so that we can avoid the IndexError
                 i = 0
                 for key in sorted(my_dict.keys()):
                     my_dict[key] = i
                     i += 1
-
+            #if the value that we are trying to delete is not existing, we are fetching the last one (it usualy is)
             elif item > max(my_dict.values()):
                 key_to_delete = max(my_dict.keys())
-                del my_dict[key_to_delete]
+                del my_dict[key_to_delete]#deletion of the searched key
 
-
+    #we store the keys of the dictionnary (the columns that remains) into a list
     columns_affected= list(my_dict.keys())
-    print("COLUMNS AFFECTED : ",columns_affected)
-    print("final matrix !!")
+    print("COLUMNS AND ROWS AFFECTED : ",columns_affected)
+    print("final matrix :")
     for i in matrix:
         print(i)
-    return matrix
+    return True, columns_affected
 
 
 
