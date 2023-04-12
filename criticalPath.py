edc892cf3dd959b03b605c0e5af153aaab8f9ae7
Guluff <88625935+Guluff@users.noreply.github.com>
2023-04-12 12:28:15 +0200

    max time constraint added

--- criticalPath.py
+++ criticalPath.py
@@ -1,7 +1,7 @@
-from latestTables import *
-from earliestTables import *
-from rank import *
-from checkValidity import *
+from Latest_stables import *
+from Earliest_stables import *
+from Rank import *
+from question3 import *
 
 
 #This function computew and returns a dictionnary containing all the latest  - earliest dates which will be used to determine the critical path
@@ -28,13 +28,14 @@ def earliestMinusLatest(n, rankDico):
 
 
 #this function fills the Critical  path array with the next vertex in the critical path
-def getNextVertex(n, vertex, rankDico, earliestMinusLatest, Critical_Path, index):
+def getNextVertex(n, vertex, rankDico, earliestMinusLatest, Critical_Path, Durations, index):
     if getSuccessors(n, int(vertex)) != []:
 
         Successors = getSuccessors(n, int(vertex))
         minRank = rankDico[str(Successors[0])]
         minRankVertex = 0
         ArrayVertex = [] # An array to store in case there are more than one critical path possible from a given vertex
+
 
         for i in Successors: #First loop to set the minimum rank among the successors and the associated vertex
             if rankDico[str(i)] <= minRank and earliestMinusLatest[str(i)] == 0:
@@ -45,15 +46,19 @@ def getNextVertex(n, vertex, rankDico, earliestMinusLatest, Critical_Path, index
             if rankDico[str(i)] == minRank and earliestMinusLatest[str(i)] == 0 and i != minRankVertex:
                 ArrayVertex.append(i)
 
+
         # if ArrayVertex is not null it means there are several critical path diverging, they are added to the Critical_Path array and computed before  continuing the initial one
         if len(ArrayVertex) >= 1:
             for i in range(len(ArrayVertex)):
                 Critical_Path.append(Critical_Path[index] + str(ArrayVertex[i]) + "->") #the new critical path is the same as the one we were working on until now therefore it is copied in the next
+                Durations.append(Durations[index] + getDurationByVertex(n, int(ArrayVertex[i])))
 
-                getNextVertex(n, ArrayVertex[i], rankDico, earliestMinusLatest, Critical_Path, len(Critical_Path) - 1)
+                getNextVertex(n, ArrayVertex[i], rankDico, earliestMinusLatest, Critical_Path, Durations, len(Critical_Path) - 1)
+
 
         Critical_Path[index] += str(minRankVertex) + "->"
-        getNextVertex(n, minRankVertex, rankDico, earliestMinusLatest, Critical_Path, index)
+        Durations[index] += getDurationByVertex(n, int(minRankVertex))
+        getNextVertex(n, minRankVertex, rankDico, earliestMinusLatest, Critical_Path, Durations, index)
     else:
         Critical_Path[index] += "Omega" # only triggers at the end of the path when there is no more successors
 
@@ -64,7 +69,7 @@ def criticalPath(n, temp_rankDico):
         if remove_zeros(getAdjacencyMatrix(n)) == False:
 
             Critical_Path = []  # Array to store if necessary the several critical paths
-
+            Durations = []
             # creates a temporary dictionnary
 
             rankDico = {} #replaces the previous dictionnary with another one that does not take in account alpha and omega for simplicity reasons
@@ -79,16 +84,26 @@ def criticalPath(n, temp_rankDico):
                 if rankDico[str(i)] == 0 and earliestMinusLatest_Dico[str(i)] == 0: #initialises the algorithm only for starting points that belong to a critical path
 
                     Critical_Path.append("Alpha->" + str(i) + "->") #Critical path is filled one vertice by one  for easier copy when needed
-                    getNextVertex(n, i, rankDico, earliestMinusLatest_Dico, Critical_Path, len(Critical_Path) - 1) #call the function to keep going
+                    Durations.append(getDurationByVertex(n, int(i)))
+                    getNextVertex(n, i, rankDico, earliestMinusLatest_Dico, Critical_Path, Durations, len(Critical_Path) - 1) #call the function to keep going
 
+            maxDuration = 0
+            for i in Durations:
+                if i > maxDuration:
+                    maxDuration = i
+
+            final_Critical_Path =  []
             for i in range(len(Critical_Path)): #displays all the critical path
+                if Durations[i] == maxDuration:
+                    final_Critical_Path.append(Critical_Path[i])
 
-                print("Critical path", i + 1, ": ", Critical_Path[i])
-
+            for i in range(len(final_Critical_Path)):
+                print("Critical path", i + 1, ": ", final_Critical_Path[i])
+            print("Duration = ", maxDuration)
         else:
             print("Cycle detected, impossible to compute a critical path")
 
-matrix = getAdjacencyMatrix(11)
-temp_rankDico = find_rank(add_column_number(del_omega(matrix)), rank=0, ranklist={})
-criticalPath(11, temp_rankDico)
 
+matrix = getAdjacencyMatrix(6)
+temp_rankDico = find_rank(add_column_number(del_omega(matrix)), rank=0, ranklist={})
+criticalPath(6, temp_rankDico)
